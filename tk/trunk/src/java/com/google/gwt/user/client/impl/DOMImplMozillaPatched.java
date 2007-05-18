131b00ae1fa5555ae8d11a503cc5b373ec599b34
mat.gessel <mat.gessel@5f4f088f-6c23-0410-9c18-f5bddeda7389>
2007-05-18 05:12:40 +0000

    Fix Mozilla error when mouseover/mouseout a TextBox or TextArea (Mozilla bug 208427)

--- tk/trunk/src/java/com/google/gwt/user/client/impl/DOMImplMozillaPatched.java
+++ tk/trunk/src/java/com/google/gwt/user/client/impl/DOMImplMozillaPatched.java
@@ -15,10 +15,13 @@
  */
 package com.google.gwt.user.client.impl;
 
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Event;
 
 /**
- * Modifications to GWT's Mozilla implementation go here. 
+ * Modifications to GWT's Mozilla implementation go here.
+ * <strong>NOTICE</strong>:
+ * portions of this file are copied from Google's DOMImplMozillaPatched: 
  */
 class DOMImplMozillaPatched extends DOMImplMozilla
 {
@@ -26,4 +29,34 @@ class DOMImplMozillaPatched extends DOMImplMozilla
 	{
 		throw new UnsupportedOperationException("Method disabled because it is unsupported on Mozilla");
 	}
+	
+	/**
+	 * @see <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=330961">Mozilla
+ 	 *      bug 330961</a>
+ 	 * @see <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=208427">Mozilla
+ 	 *      bug 208427</a>
+	 */
+	public native boolean isOrHasChild(Element parent, Element child) /*-{
+		while (child)
+		{
+			if (parent.isSameNode(child))
+			{
+				return true;
+			}
+			// Workaround for Mozilla bug # 
+			try
+			{
+				child = child.parentNode;
+				if (child.nodeType != 1)
+				{
+					child = null;
+				}
+			}
+			catch (e)
+			{
+				child = null;
+			}
+		}
+		return false;
+	}-*/;
 }
